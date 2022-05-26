b612496e5f68172036cbcc9e3242daaaf878cdac
mytranbui <mytranbui@hotmail.com>
2022-05-26 23:33:38 +0200

    all ok except get amount

--- shape_calculator.py
+++ shape_calculator.py
@@ -27,10 +27,7 @@ class Rectangle:
       return "Too big for picture."
     j = 0
     while j < self.height:
-      picture = '*' * self.width + "\n"
-      # i = 0
-      # while i < self.width:
-      #   i += 1
+      picture += "*" * self.width + "\n"
       j += 1
     return picture
 
@@ -47,15 +44,48 @@ class Rectangle:
 
 class Square(Rectangle):
   def __init__(self, side):
-    self.height = side
-    self.width = side
-    self.side = side
+    super().__init__(side, side)
   
   def set_side(self, side):
-    self.side = side
+    self.set_width(side)
+    self.set_height(side)
+    # self.side = side
   
+  def set_width(self,side):
+    super().set_width(side)
+    super().set_height(side)
+    # self.side = side
+
+  def set_height(self, side):
+    super().set_height(side)
+    super().set_width(side)
+    # self.side = side
+
   def __str__(self):
     string = ""
-    string = "Square(side=" + str(self.side) + ")"
+    string = "Square(side=" + str(self.height) + ")"
     return string
 
+rect = Rectangle(5, 10)
+print(rect.get_area())
+rect.set_width(3)
+print(rect.get_perimeter())
+print(rect)
+rect.set_height(7)
+print(rect)
+print(rect.get_picture())
+
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
+
+sq.set_width(2)
+print(sq)
+print(sq.get_picture())
+
+sq.set_height(8)
+print(sq)
+print(sq.get_picture())
